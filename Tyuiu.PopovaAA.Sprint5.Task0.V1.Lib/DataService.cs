8a49bdf9c835ec89173ca74141fbe901f2a26c9c
Tyuiu.PopovaAA <Tyuiu.PopovaAA@outlook.com>
2024-11-30 02:46:02 +0500

    0

--- Tyuiu.PopovaAA.Sprint5.Task0.V1.Lib/DataService.cs
+++ Tyuiu.PopovaAA.Sprint5.Task0.V1.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.PopovaAA.Sprint5.Task0.V1.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = $@"{Path.GetTempPath()}\OutPutFileTask0.txt";
             
             double y = (double)Math.Pow(x, 3) + ((double)2 * Math.Pow(x, 2)) + ((double)5 * x) + (double)4;
           
